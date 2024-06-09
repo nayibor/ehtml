98a8fe528fad56d46b574ee53a0fd771172bad7b
Nuku Ameyibor <nayibor@gmail.com>
2024-06-09 19:24:16 +0000

    type specs for ehtml added and some functions removed from api
    type specs added for  ehtml in yaws_ehtml.erl which are exported as opaque types
    functions for using ehtml with templates removed from ehtml.erl api
    types added to api file
    readme edited to removed parts about using templates

--- src/ehtml.erl
+++ src/ehtml.erl
@@ -1,27 +1,15 @@
 -module(ehtml).
 
--export([ehtml_expand/1,ehtml_expander/1,ehtml_apply/2]).
+-export([ehtml_expand/1]).
 -export([h2e/1]).
 
-
+-spec ehtml_expand(yaws_ehtml:ehtml()) -> iolist().
 %% @doc This is for expanding erlang terms into html based on erlang input terms.
 %% @end
 ehtml_expand(Ch)->
     yaws_ehtml:ehtml_expand(Ch).
-    
 
-%% @doc  This is an expander that can be used for creating optimized ehtml template.
-%% @end
-ehtml_expander(Ch)->
-    yaws_ehtml:ehtml_expander(Ch).
-
-
-%% @doc This is a template applyer that takes a template and a proplist and generates the html
-%% @and 
-ehtml_apply(Expander,Env)->
-    yaws_ehtml:ehtml_apply(Expander, Env).
-
-
+-spec h2e(list() | binary) -> yaws_ehtml:ehtml().
 %% @doc this function is for converting html into ehtml giving the input binary or string
 h2e(Input)->
     yaws_html:h2e(Input).
