9865eb3660359e0b830d1ca86a2dbaf3dd02e2d3
Nuku Ameyibor <nayibor@gmail.com>
2024-06-10 13:37:37 +0000

    input type specification in h2e function changed to list | binary in ehtml:h2e/1
    the input type specification changed to list | binary in ehtml:h2e/1

--- src/ehtml.erl
+++ src/ehtml.erl
@@ -9,7 +9,7 @@
 ehtml_expand(Ch)->
     yaws_ehtml:ehtml_expand(Ch).
 
--spec h2e(list() | binary) -> yaws_ehtml:ehtml().
+-spec h2e(list() | binary()) -> yaws_ehtml:ehtml().
 %% @doc this function is for converting html into ehtml giving the input binary or string
 h2e(Input)->
     yaws_html:h2e(Input).
